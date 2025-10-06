76d1354fce30251d7bf29facd3e16b53741d924f
dimah <hammyghareghani@gmail.com>
2025-10-06 10:57:11 +0330

    exercises: add 1 exercise to OOP_programming_in_python

---
+++ OOP_programming_in_python/animal_management_system.py
@@ -0,0 +1,46 @@
+class Animal():
+    
+    zoo_name = "Jadi's zoo"
+    
+    def __init__(self, name, species, age, sound):
+        self.name = name
+        self.species = species
+        self.age = age
+        self.sound = sound
+        
+    def __str__(self):
+        return f"{self.name}, {self.species}, {self.age}, {self.sound}"
+    
+    def info(self):
+        print(f"Zoo: {Animal.zoo_name}\n")
+        print("Animal:")
+        print(f"Name: {self.name}")
+        print(f"Species: {self.species}")
+        print(f"Age: {self.age}")
+        print(f"Sound: {self.sound}")
+        
+    def make_sound(self):
+        return f"{self.name} is {self.sound}ing"
+
+class Bird(Animal):
+    
+    def __init__(self, name, species, age, sound, wind_span):
+        Animal.__init__(self, name, species, age, sound)
+        self.wind_span = wind_span
+        
+    def make_sound(self):
+        return f"{self.name} is chirp like {self.sound}"
+    
+    
+animal = Animal('Lion', 'mammal', 8, "grrrruaaaalllll")
+
+print(animal.make_sound())
+
+print()
+
+animal.info()
+
+print()
+
+print(animal)
+        
