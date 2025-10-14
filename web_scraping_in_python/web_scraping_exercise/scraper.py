f85db6608b88d2a0248729abb4586c161a6303e5
dimah <hammyghareghani@gmail.com>
2025-10-14 12:03:02 +0330

    feat: add 1 exercise to web_scraping_in_python/

---
+++ web_scraping_in_python/web_scraping_exercise/scraper.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://taaghche.com/audiobook/165476/%D8%A7%D9%88%D9%84-%D8%B9%D8%A7%D8%B4%D9%82-%D8%AE%D9%88%D8%AF%D8%AA-%D8%A8%D8%A7%D8%B4"
+req = requests.get(url)
+
+req.raise_for_status()
+
+soup = BeautifulSoup(req.text, "html.parser")
+
+image = soup.select_one("#book-image")
+title = soup.select_one(".bookHeader_bookTitleContainer__7Z98p > h1:nth-child(1)")
+author = soup.select_one("div.bookHeader_bookInfo__panht:nth-child(1) > span:nth-child(2) > a:nth-child(1) > span:nth-child(1)")
+price = soup.select_one(".format_priceNum__KA_Sh")
+category = soup.select_one(".categories_link__aXiRr")
+
+print("Title: ", title.text.strip())
+print("Author: ", author.text.strip())
+print("Image URL: ", image["src"])
+print("Category: ", category.text.strip())
+
+
